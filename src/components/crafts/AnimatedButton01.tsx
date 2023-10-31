b34bdc2ecc4c0e83f871c5d875737c990e027eb1
Kartik Khorwal <hellokartikk@gmail.com>
2023-11-01 01:17:36 +0530

    add: twisted button

--- src/components/crafts/AnimatedButton01.tsx
+++ src/components/crafts/AnimatedButton01.tsx
@@ -1,10 +1,44 @@
+"use client"
+
 import React from 'react'
 
 function AnimatedButton01() {
 
+  const fullWord = 'Subscribe'
+  const word = fullWord.split('')
+
   return (
     <div>
-      <button className='bg-primary text-white px-6 p-2 rounded-full'>Welcome</button>
+      <button className='bg-primary group flex items-center overflow-hidden text-white px-6 h-12 rounded-full'>
+        <span className='inline-flex items-center relative overflow-hidden h-[14px]'>
+          {word.map((letter, index) => (
+            <span
+              key={index}
+              className={`inline-block group-hover:-translate-y-8 duration-[600ms] ease-in-out`}
+              style={{
+                transitionDelay: `${index * 30}ms`,
+              }}
+            >
+              {letter}
+            </span>
+          ))}
+
+          <span className='inline-flex left-0 absolute top-0 h-[14px] items-center'>
+            {word.map((letter, index) => (
+              <span
+                key={index}
+                className={`inline-block translate-y-[18px] group-hover:-translate-y-[0px] duration-[600ms] ease-in-out`}
+                style={{
+                  transitionDelay: `${index * 30}ms`,
+                }}
+              >
+                {letter}
+              </span>
+            ))}
+
+          </span>
+        </span>
+      </button>
     </div>
   )
 
